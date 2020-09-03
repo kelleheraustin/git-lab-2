6f09546f17311eeb009e75596171c2577baaf221
Austin Kelleher <ak976418@ohio.edu>
2020-09-03 16:00:19 -0400

    c++ program

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Austin Kelleher
+ *        Date: September 3, 2020
+ * Description: Step 15 of Week 2 Lab
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    
+    return 0;
+}// main
